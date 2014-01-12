79d10ad8c4b1758e263b68401f4e79077669c586
Adrian Coman <adr.coman@gmail.com>
2014-01-12 16:33:46 +0200

    something something

--- particle.cpp
+++ particle.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 #include <vector>
 #include <algorithm>
@@ -63,6 +65,9 @@ void SortParticles(){
 
 int main( void )
 {
+	
+	srand(time(0));
+
 	// Initialise GLFW
 	if( !glfwInit() )
 	{
